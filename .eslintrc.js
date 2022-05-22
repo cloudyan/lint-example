ecaa98fe35b90e44f3dbb71848a01e84c6178290
cloudyan <1395093509@qq.com>
2022-05-22 22:29:08 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,239 @@
+const isTsProject = false
+
+module.exports = {
+  root: true,
+  // requireConfigFile: false,
+  // project: './tsconfig.json',
+
+  parser: isTsProject ? '@typescript-eslint/parser' : '@babel/eslint-parser',
+
+  // parser: 'vue-eslint-parser',
+  parserOptions: {
+    // parser: 'babel-eslint',
+    // parser: {
+    //   // Script parser for `<script>`
+    //   js: 'espree', // babel-eslint espree
+
+    //   // Script parser for `<script lang="ts">`
+    //   ts: '@typescript-eslint/parser',
+
+    //   // Script parser for vue directives (e.g. `v-if=` or `:attribute=`)
+    //   // and vue interpolations (e.g. `{{variable}}`).
+    //   // If not specified, the parser determined by `<script lang ="...">` is used.
+    //   '<template>': 'espree',
+    // },
+    sourceType: 'module', // 默认 script
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true, // 用于支持 jsx
+    },
+    // presets: [
+    //   '@babel/preset-env',
+    //   '@babel/preset-react',
+    //   '@babel/preset-typescript',
+    // ],
+    // plugins: [
+    //   // 此处顺序不能变，具体参看文档
+    //   // https://babel.dev/docs/en/babel-plugin-proposal-decorators
+    //   ['@babel/plugin-proposal-decorators', { legacy: true }],
+    //   ['@babel/plugin-proposal-class-properties', { loose: true }],
+    // ],
+  },
+
+  extends: [
+    // 'eslint-config-airbnb-base',
+    'prettier',
+    // 'prettier/react', // has been merged into "prettier" in eslint-config-prettier 8.0.0
+  ]
+    .concat
+    // isTsProject
+    //   ? [
+    //       // 'prettier/@typescript-eslint', // has been merged into "prettier" in eslint-config-prettier 8.0.0
+    //       'plugin:@typescript-eslint/recommended',
+    //     ]
+    //   : ['plugin:react/recommended'],
+    (),
+  // plugins: [
+  //   'eslint-comments',
+  //   // 'import', // 暂不开启
+  //   'react',
+  //   // 'jsx-a11y', // 这个暂时不必要
+  //   // 'promise',
+  //   'jest',
+  //   'unicorn', // 可强制约束文件命名格式，默认 kebabCase 格式
+  //   'react-hooks',
+  //   'markdown', // 需要 .eslintrc.js 配置 root: true
+  // ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  globals: {
+    // writable readonly off
+    window: 'readonly',
+    document: 'readonly',
+    expect: 'readonly',
+    sinon: 'readonly',
+    Blob: 'readonly',
+    URL: 'readonly',
+    isNaN: 'readonly',
+    uni: 'readonly',
+    Promise: 'readonly',
+    getCurrentPages: 'readonly',
+    crypto: 'readonly',
+    Uint8Array: 'readonly',
+    Set: 'readonly',
+    bridge: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  settings: {
+    // support import modules from TypeScript files in JavaScript files
+    'import/resolver': {
+      node: {
+        extensions: isTsProject ? ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] : ['.js', '.jsx'],
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+  },
+  rules: {
+    // ...(isTsProject ? tsEslintConfig : {}),
+    // ...jsEslintConfig,
+
+    // 'vue/multi-word-component-names': 'off',
+    // // '@typescript-eslint/no-explicit-any': 'off',
+    // // '@typescript-eslint/ban-type': 'off',
+    // // https://yepbug.com/2018/08/28/what-is-the-benefit-of-prefer-default-export/
+    // // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/README.md
+    // 'import/prefer-default-export': 'off', // 输出只有一个变量时使用 export default
+    // // 因为 mac 系统默认不区分大小写, 所以建议使用全小写, 包含文件名 文件夹名 组件名(html 特性也不区分大小写)
+    // 'vue/component-definition-name-casing': ['error', 'kebab-case'],
+    // 'vue/name-property-casing': ['error', 'kebab-case'], // 'PascalCase' |'kebab-case'
+    // // 'vue/max-attributes-per-line': ['error', {
+    // //   'singleline': 4,
+    // //   'multiline': {
+    // //     'max': 1,
+    // //     'allowFirstLine': false,
+    // //   },
+    // // }],
+    // // 'vue/html-indent': 'error', // 此条与 Prettier 冲突，需要移除
+    // 'vue/script-setup-uses-vars': 'error',
+    // 'vue/multiline-html-element-content-newline': 'off',
+    // 'vue/no-v-html': 'off',
+    // 'vue/no-template-shadow': 'off', // temp
+    // 'vue/order-in-components': 'off', // temp
+    // 'vue/require-default-prop': 'off',
+    // // 'vue/no-unused-components': '1',
+    // 'vue/singleline-html-element-content-newline': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    // 'max-len': [
+    //   'error',
+    //   {
+    //     code: 120,
+    //     ignoreUrls: true,
+    //     ignorePattern: true,
+    //   },
+    // ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-else-return': 'off',
+    'no-mixed-operators': 'off',
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: true,
+      },
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 2,
+        maxEOF: 1,
+      },
+    ],
+    'no-nested-ternary': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': [
+      'off',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-restricted-syntax': 'off',
+    'no-shadow': [
+      'error',
+      {
+        allow: ['res', 'data', 'err', 'cb', 'state', 'resolve', 'reject', 'done'],
+      },
+    ],
+    'no-trailing-spaces': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-unused-vars': 'off',
+    // 'no-unused-vars': [
+    //   'error',
+    //   {
+    //     vars: 'all',
+    //     // args: 'after-used',
+    //     args: 'none',
+    //     caughtErrors: 'none',
+    //     ignoreRestSiblings: true,
+    //   },
+    // ],
+    'no-use-before-define': 'off',
+    'no-useless-escape': 'off',
+    'prefer-template': 'off',
+    'prefer-arrow-callback': 'off',
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'require-yield': [1],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'ignore',
+        asyncArrow: 'ignore',
+      },
+    ],
+    semi: ['off', 'never'],
+    'func-names': 'off',
+    'consistent-return': 'off',
+  },
+}
--- README.md
+++ README.md
@@ -4,9 +4,11 @@ lint example
 
 - 项目中如何接入
 - IDE 编辑器如何接入
+  - `"editor.formatOnSave": true,`
+  - 解决 Prettier 和 ESLint 冲突
 - CI 流程如何接入
 
-## lint 列表
+## 项目中接入 lint
 
 接入步骤
 
@@ -19,7 +21,12 @@ lint example
 7. typecheck
 8. eslint
 9. stylelint
-10. conventional-changelog
+10. browserlist
+11. conventional-changelog
+
+### 如何执行落地？
+
+集成到 vscode, webpack 以及 CI 流程上。
 
 ### .npmrc && .nvmrc
 
@@ -167,7 +174,7 @@ module.exports = {
       ],
     ],
   },
-};
+}
 ```
 
 测试
@@ -206,6 +213,9 @@ TODO
 
 config
 
+> Unix-style newlines with a newline ending every file
+> 根目录的配置文件，编辑器会由当前目录向上查找，如果找到 `roor = true` 的文件，则不再查找
+
 ```ini
 # .editorconfig
 # https://editorconfig.org/
@@ -237,11 +247,100 @@ quote_type = single
 
 ### eslint
 
+接入 eslint
+
+- 按照 prettier 原则，尽量减少格式化对开发的干扰
+  - 不应该因为尾分号分心，而交给格式化工具自动处理，此时 eslint 应关闭规则
+
+```bash
+# base
+npm i -D eslint @babel/eslint-parser
+# prettier
+npm i -D eslint-config-prettier
+npm i -D eslint-plugin-import
+
+# error  Parsing error: No Babel config file detected for xxx.js. Either disable config file checking with requireConfigFile: false, or configure Babel so that it can find the config files
+# 缺少 babel 配置, 添加 babel.config.js 后 OK
+
+# airbnb
+npm i -D eslint-config-airbnb-base
+# xo
+npm i -D eslint-formatter-pretty
+
+# plugin
+npm i -D eslint-plugin-babel eslint-plugin-eslint-comments
+npm i -D eslint-plugin-compat eslint-plugin-markdown eslint-plugin-promise eslint-plugin-unicorn
+# test
+npm i -D eslint-plugin-jest
+
+# ts
+# plugin:@typescript-eslint/recommended
+npm i -D @typescript-eslint/parser @typescript-eslint/recommended @typescript-eslint/eslint-plugin
+
+# react
+# plugin:react/recommended
+npm i -D eslint-plugin-jsx-a11y
+npm i -D eslint eslint-plugin-react eslint-plugin-react-hooks
+
+# vue
+npm i -D eslint-plugin-import eslint-plugin-vue
+
+```
+
+config
+
+```js
+
+```
+
+### babel
+
+```bash
+npm i -D @babel/core @babel/preset-env
+```
+
 ### stylelint
+
+接入 stylelint
+
+```bash
+npm i -D stylelint stylelint-config-css-modules stylelint-config-prettier stylelint-config-rational-order stylelint-config-standard stylelint-declaration-block-no-ignored-properties stylelint-no-unsupported-browser-features stylelint-order
+
+# prettier
+npm i -D prettier-plugin-jsdoc prettier-plugin-style-order
+```
+
+### browserlist
+
+```json5
+  "browserslist": [
+    "> 1%",
+    "last 2 versions",
+    "not dead",
+    "Android >= 4",
+    "ios >= 8"
+  ],
+
+// 或
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  },
+```
 
 ### conventional-changelog
 
-usage
+Commit 规范化之后，就可以通过工具把关键信息找出来，自动生成到 CHANGELOG 中。
+
+conventional-changelog 是一款可以根据项目的 commit 和 metadata 信息自动生成 changelogs 和 release notes 的系列工具，并且在辅助 [standard-version](https://github.com/conventional-changelog/standard-version) 工具的情况下，可以自动帮你完成生成 version、打 tag, 生成 CHANGELOG 等系列过程。
 
 ```bash
 npm i conventional-changelog-cli -D
@@ -254,6 +353,62 @@ config
   "changelog": "conventional-changelog -p angular -i CHANGELOG.md -s",
 }
 ```
+
+- [Commit message 和 Change log 编写指南](https://www.ruanyifeng.com/blog/2016/01/commit_message_change_log.html)
+- https://zhuanlan.zhihu.com/p/51894196
+
+## IDE 编辑器接入 lint
+
+- prettier 是一个校验代码格式化的工具
+- ESlint 是校验语法的工具
+
+VSCode 相关插件
+
+- [ESLint 插件](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+- [Prettier - Code formatter 插件](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
+- [Prettier ESLint 插件](https://marketplace.visualstudio.com/items?itemName=rvest.vs-code-prettier-eslint)
+
+```json
+{
+  "eslintConfig": {
+    "extends": "react-app",
+    "plugins": ["prettier"],
+    "rules": {
+      "prettier/prettier": "error"
+    }
+  }
+}
+```
+
+package.json 中直接添加插件 prettier, 这样 ESlint 的窗口也会直接输出 prettier 的报错信息。
+
+为了自动格式化 prettier 的错误也需要添加相应的规则。
+
+ESlint 集成了 prettier 的校验规则，因此 VSCode 中不需要再单独安装 prettier 插件。
+
+## 常见问题
+
+### 解决 Prettier 和 ESLint 冲突
+
+vscode 配置了在文件保存时进行格式化和 ESLint 自动修复：
+
+```json
+  "editor.formatOnSave": true, // 保存时自动格式化
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true // 保存时使用eslint校验文件
+  },
+  "editor.defaultFormatter": "esbenp.prettier-vscode", // 格式化时使用 prettier
+```
+
+保存文件时，ESLint 先 fix 了代码，之后 prettier 格式化了代码，导致代码变得不符合 ESLint 规则了。
+
+解决方案，使用 `prettier-eslint`
+
+`prettier-eslint` 是先把代码用 prettier 格式化，然后再用 ESLint fix。这和 vscode 保存文件时的流程是相反的。
+
+eslint-plugin-prettier 插件用 prettier 替代了 eslint 本身对于代码美化部分的功能，而其中的配置是官方默认配置，并且不从.prettierrc 文件中读取配置
+
+当.prettierrc 的配置和官方默认配置不一致的时候，就会出现上面那种问题。
 
 ## 源代码
 
