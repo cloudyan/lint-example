f1baa5320a45a06c179883b0684b6d30b063fa68
cloudyan <1395093509@qq.com>
2022-05-23 00:37:12 +0800

    ci: add lint-staged eslint:fix

--- src/react-example.js
+++ src/react-example.js
@@ -1,21 +1,22 @@
 // 测试校验 lint 规则
-import { useState } from 'react';
+import { useState } from 'react'
 
-console.log(useState);
+console.log(useState)
 
-const noop = (...rest) => { console.log(rest); };
-export const sumNumber = noop(1, 2);
+const noop = (...rest) => {
+  console.log(rest)
+}
+export const sumNumber = noop(1, 2)
 
-const x = 1;
-console.log(x);
+const x = 1
+console.log(x)
 
+export const obj1 = { foo: ['baz', 'bar'] } // 这里期望 ]}
+export const obj2 = { foo: { baz: 1, bar: 2 } } // 这里期望 }}
+export const arr1 = [['foo'], 'bar']
+noop({ name: 'red', age: 20 })
 
-export const obj1 = { foo: ['baz', 'bar'] };     // 这里期望 ]}
-export const obj2 = { foo: { baz: 1, bar: 2 } }; // 这里期望 }}
-export const arr1 = [['foo'], 'bar'];
-noop({ name: 'red', age: 20 });
-
-const props = { name: 'red', age: 20 };
-const { age = 20 } = props;
-const { foo = [] } = obj1;
-const [a1, b1] = arr1;
+const props = { name: 'red', age: 20 }
+const { age = 20 } = props
+const { foo = [] } = obj1
+const [a1, b1] = arr1
