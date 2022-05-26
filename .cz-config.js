2fbc9c299d6ded1717bc4b610c35b5c7dae67359
cloudyan <1395093509@qq.com>
2022-05-26 18:38:41 +0800

    build: devDeps -> deps

--- .cz-config.js
+++ .cz-config.js
@@ -4,16 +4,18 @@
 module.exports = {
   // prettier-ignore
   types: [
-    {value: 'feat',     name: '特性: 一个新的特性'},
-    {value: 'fix',      name: '修复: 修复一个Bug'},
-    {value: 'docs',     name: '文档: 变更的只有文档'},
-    {value: 'style',    name: '格式: 空格, 分号等格式修复'},
-    {value: 'refactor', name: '重构: 代码重构，注意和特性、修复区分开'},
-    {value: 'pref',     name: '性能: 提升性能'},
-    {value: 'test',     name: '测试: 添加一个测试'},
-    {value: 'chore',    name: '工具: 开发工具变动(构建、脚手架工具等)'},
-    {value: 'revert',   name: '回滚: 代码回退'},
-    {value: 'WIP',      name: 'WIP: 工作中'}
+    {value: 'feat',     name: 'feat     : 一个新的特性'},
+    {value: 'fix',      name: 'fix      : 修复一个Bug'},
+    {value: 'docs',     name: 'docs     : 变更的只有文档'},
+    {value: 'style',    name: 'style    : 空格, 分号等格式修复'},
+    {value: 'refactor', name: 'refactor : 代码重构，注意和特性、修复区分开'},
+    {value: 'pref',     name: 'pref     : 提升性能'},
+    {value: 'test',     name: 'test     : 添加一个测试'},
+    {value: 'build',    name: 'build    : 编译相关变更(发布版本、依赖改动)'}, // 新增
+    {value: 'chore',    name: 'chore    : 开发工具变动(构建、脚手架工具等)'},
+    {value: 'revert',   name: 'revert   : 代码回退'},
+    {value: 'ci',       name: 'ci       : CI 相关变更'}, // 新增
+    {value: 'WIP',      name: 'WIP      : 工作中'}
   ],
   // prettier-ignore
   scopes: [
