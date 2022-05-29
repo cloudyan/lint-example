b1bf8c1fc6ccace6c6610e030f80444f7d320e14
cloudyan <1395093509@qq.com>
2022-05-29 23:17:52 +0800

    fix: stylelint `configBasedir`, npm 改为 yarn

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,9 @@
 // .stylelintrc.js
+const path = require('path');
 const { getStylelintConfig } = require('@applint/spec');
 
 // getStylelintConfig(rule: 'common' | 'rax' | 'react' | 'vue',  customConfig?: StylelintConfig);
 module.exports = getStylelintConfig('react', {
   // extends: ['prettier'],
+  configBasedir: path.dirname(require.resolve('@applint/spec')),
 });
