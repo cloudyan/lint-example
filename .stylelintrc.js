268f87566d6b23033dfbfe0a513bce92a8d8cd16
cloudyan <1395093509@qq.com>
2022-05-30 00:06:13 +0800

    chore: use npm

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,7 @@
 // .stylelintrc.js
-const path = require('path');
 const { getStylelintConfig } = require('@applint/spec');
 
 // getStylelintConfig(rule: 'common' | 'rax' | 'react' | 'vue',  customConfig?: StylelintConfig);
 module.exports = getStylelintConfig('react', {
   // extends: ['prettier'],
-  configBasedir: path.dirname(require.resolve('@applint/spec')),
 });
